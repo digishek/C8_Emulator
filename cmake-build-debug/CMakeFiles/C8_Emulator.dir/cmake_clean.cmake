c43f176684c293ef35fbcb47e8b6ceb9c5398fa9
digishek <digishek@gmail.com>
2021-11-05 13:46:09 +0530

    CPU Commands done

---
+++ cmake-build-debug/CMakeFiles/C8_Emulator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "C8_Emulator"
+  "C8_Emulator.pdb"
+  "CMakeFiles/C8_Emulator.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C8_Emulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
